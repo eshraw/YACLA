054953459f92f9522ba665ee4710631e10b02057
Eshane <rawateshane@gmail.com>
2024-10-01 14:31:48 +0200

    Project init - basic set up

---
+++ yet_another_climbing_log_app/android/app/src/main/kotlin/com/example/yet_another_climbing_log_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.yet_another_climbing_log_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ yet_another_climbing_log_app/lib/main.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'database_helper.dart';
+
+void main() {
+  runApp(const YetAnotherClimbingLogApp());
+}
+
+class YetAnotherClimbingLogApp extends StatelessWidget {
+  const YetAnotherClimbingLogApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Yet Another Climbing Log App'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  List<Map<String, dynamic>> _items = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _refreshItems();
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+    _addItem();
+  }
+
+  Future<void> _addItem() async {
+    await DatabaseHelper.instance.insertItem({
+      'name': 'Item $_counter',
+      'quantity': _counter,
+    });
+    _refreshItems();
+  }
+
+  void _refreshItems() async {
+    final data = await DatabaseHelper.instance.queryAllItems();
+    setState(() {
+      _items = data;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _items.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(_items[index]['name']),
+                    subtitle: Text('Quantity: ${_items[index]['quantity']}'),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
