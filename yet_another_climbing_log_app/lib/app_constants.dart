9a01d1ad3d1a6208d7f7f4dc6ee60d54df78d00a
Eshane <rawateshane@gmail.com>
2024-10-19 22:38:38 +0200

    Added the app constant font grades

---
+++ yet_another_climbing_log_app/lib/app_constants.dart
@@ -0,0 +1,11 @@
+class AppConstants {
+  static const List<String> CLIMBING_GRADES = [
+    '3', '4', '5A', '5A+', '5B',
+    '5B+', '5C', '5C+', '6A',
+    '6A+', '6B', '6B+', '6C',
+    '6C+', '7A', '7A+', '7B',
+    '7B+', '7C', '7C+', '8A',
+    '8A+', '8B', '8B+', '8C',
+    '8C+', '9A', '9A+', '9B'
+  ];
+}
